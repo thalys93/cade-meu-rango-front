621a00598756f6faab969873944d3f2faae51c36
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-05-19 22:09:46 -0300

    Camadas da Aplicação
    - Criado o Contexto do Usuário Autenticado
    - Criado as Camadas a Aplicação
     - Dark Mode
      - Dev
       - Usuário
         - Rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Routes from './routes'
+
 import { DarkModeProvider } from './utils/api/context/darkModeContext/DarkModeContext'
+import { DeveloperProvider } from './utils/api/context/devContext/DevContext'
+import { UserAutenticatedProvider } from './utils/api/context/userAutenticatedContext/isLogged'
 
 // Import do CSS
 import './interface/css/global.css'
 import './interface/css/respon.css'
 import './interface/css/darkmode.css'
 import 'animate.css'
-import { DeveloperProvider } from './utils/api/context/devContext/DevContext'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(  
-    <DarkModeProvider>
-      <DeveloperProvider>
-        <Routes />
+    <DarkModeProvider>  {/* Camada do Modo Escuro */}
+      <DeveloperProvider> {/* Camada de Desenvolvedor (temporário) */}
+        <UserAutenticatedProvider> {/* Camada de Usuário */}
+          <Routes />
+        </UserAutenticatedProvider>
       </DeveloperProvider>
-    </DarkModeProvider>  
+    </DarkModeProvider> 
 )
---
+++ src/utils/api/context/userAutenticatedContext/isLogged.jsx
@@ -0,0 +1,29 @@
+import React, {createContext, useState, useEffect} from "react";
+
+const UserAutenticatedContext = createContext();
+
+const UserAutenticatedProvider = ({children}) => {
+    const {isLogged, setIsLogged} = useState(false);
+
+    useEffect(() => {
+        const storedMode = localStorage.getItem('isLogged');
+        if (storedMode) {
+            setIsLogged(JSON.parse(storedMode));
+        }
+    }, []);
+
+    // Ativar o Usuário (a ideia é que ele busque o usuário na api e valide o login)
+    // const toggleUserAutenticated = () => {
+    //     const userAutenticated = !isLogged;
+    //     setIsLogged(userAutenticated);
+    //     localStorage.setItem('userAutenticated', JSON.stringify(userAutenticated));
+    // }
+    
+    return(
+        <UserAutenticatedContext.Provider value={{isLogged}}>
+            {children}
+        </UserAutenticatedContext.Provider>
+    );
+}
+
+export {UserAutenticatedContext, UserAutenticatedProvider};
